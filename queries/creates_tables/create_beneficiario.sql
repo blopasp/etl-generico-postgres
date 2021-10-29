c92c10a6cb452eea657bbb46f976425f67b3f2e2
blopasp <pabloandreson@gmail.com>
2021-10-29 18:58:35 -0300

    commit

--- queries/creates_tables/create_beneficiario.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS public.beneficiario(
-    codigo_beneficiario serial primary key,
-    nome varchar(255),
-    codigo_localidade int,
-    CONSTRAINT fk_loc FOREIGN KEY (codigo_localidade) REFERENCES public.localidade(codigo_localidade)
-);
